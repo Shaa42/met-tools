ed7d8cd92720459b2b63a5f33b30c5bac16c0bad
Shaa42 <ryanvigoureux@hotmail.com>
2025-11-04 14:35:34 +0100

    first commit

---
+++ geoloc/get_loc_ipv4.py
@@ -0,0 +1,56 @@
+import json
+import subprocess
+from time import sleep
+
+
+def callBashScript(fname: str):
+    print("Calling script...")
+    _ = subprocess.run(
+        ["bash", "resolve_ip_traceroute.sh", fname], text=True, capture_output=True
+    )
+    sleep(1)
+    print("Done.")
+
+
+if __name__ == "__main__":
+    resolve = True
+    fname_ext = "example"
+    if resolve:
+        callBashScript(fname_ext)
+    fname = "assets/trcrt-out/fanrt_" + fname_ext + ".txt"
+
+    ip_list: list[str] = []
+    with open(fname, "r") as file:
+        for line in file:
+            ip = line.strip()
+            if ip and ip not in ip_list:
+                ip_list.append(ip.strip("*"))
+
+    # print(ip_list)
+
+    loc_dict = {}
+    for ip in ip_list:
+        # Call ipinfo.io API
+        json_output = subprocess.run(
+            ["bash"], input=f"curl ipinfo.io/{ip}", text=True, capture_output=True
+        ).stdout
+
+        # print(json_output)
+
+        # Get "loc" value and put it in loc_list
+        data = json.loads(json_output)
+        if "loc" in data:
+            loc_dict[ip] = data["loc"]
+        else:
+            print(f"Didn't add loc of : {data['ip']}")
+
+    # print("List of the localisations of each IPv4 address of the traceroute :")
+    # print(loc_dict)
+
+    # Create CSV file based on loc_dict
+    print("Creating CSV file...")
+    with open("loc_ipv4_fandom.csv", "w") as file:
+        file.write("ip,latitude,longitude\n")
+        for ip, loc in loc_dict.items():
+            file.write(f"{ip},{loc}\n")
+    print("Done.")
---
+++ geoloc/resolve_ip_traceroute.sh
@@ -0,0 +1,3 @@
+name="$1"
+
+sudo traceroute -n --tcp fandom.com | tail -n+2 | awk '{ ip=""; for(i=1;i<=NF;i++) if ($i ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}$/) { ip=$i; break } print ip }' > assets/trcrt-out/fanrt_"$name".txt

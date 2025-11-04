b27c8fca5cb479b328be7ac3f30d026766d00b2c
Shaa42 <ryanvigoureux@hotmail.com>
2025-11-04 17:01:09 +0100

    Add: sendwaits arg

--- geoloc/resolve_ip_traceroute.sh
+++ geoloc/resolve_ip_traceroute.sh
@@ -1,3 +1,3 @@
 name="$1"
 
-sudo traceroute -n --tcp fandom.com | tail -n+2 | awk '{ ip=""; for(i=1;i<=NF;i++) if ($i ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}$/) { ip=$i; break } print ip }' > assets/trcrt-out/fanrt_"$name".txt
+sudo traceroute -n --tcp --sendwait=0.5 fandom.com | tail -n+2 | awk '{ ip=""; for(i=1;i<=NF;i++) if ($i ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}$/) { ip=$i; break } print ip }' > assets/trcrt-out/fanrt_"$name".txt
